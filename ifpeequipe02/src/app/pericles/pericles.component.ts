53ba66ab0ea4624909e2932894f866fe78089b2e
pericles <pericless-soares@outlook.com>
2019-09-11 08:56:38 -0300

    criei um componente com meu nome

---
+++ ifpeequipe02/src/app/pericles/pericles.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pericles',
+  templateUrl: './pericles.component.html',
+  styleUrls: ['./pericles.component.css']
+})
+export class PericlesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
