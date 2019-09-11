cb761bd4a3747a884046a8244f884f3be568daaa
cllsabino <carlossabino268@gmail.com>
2019-09-11 08:57:00 -0300

    criei um component com o meu nome

---
+++ ifpeequipe02/src/app/carlos/carlos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-carlos',
+  templateUrl: './carlos.component.html',
+  styleUrls: ['./carlos.component.css']
+})
+export class CarlosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
