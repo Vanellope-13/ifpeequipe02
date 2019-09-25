c4cac6554fd228184e311a83802582b54bf5e16e
Vanellope-13 <yasmim.adrieny@gmail.com>
2019-09-25 16:52:14 -0300

    fiz a tela-final

---
+++ ifpeequipe02/src/app/tela-final/tela-final.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tela-final',
+  templateUrl: './tela-final.component.html',
+  styleUrls: ['./tela-final.component.css']
+})
+export class TelaFinalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
